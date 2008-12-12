03acef14bfe7da39bb4777b66902b435a7d1bf22
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-12 14:46:42 +0000

    import plugins and sem-theme
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/uploads-folder/trunk@1 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ readme.txt
@@ -0,0 +1,29 @@
+=== Uploads Folder ===
+Contributors: Denis-de-Bernardy, Semiologic
+Donate link: http://buy.semiologic.com/drink
+Tags: uploads, uploads-folder, uploads folder, cms
+Requires at least: 2.5
+Tested up to: 2.6.5
+Stable tag: trunk
+
+The Uploads Folder plugin for WordPress changes your uploads folders to a more natural uploads/yyyy/mm for posts (based on the post's date rather than the current date), and uploads/page/sub-page for static pages (based on the page's position in the hierarchy).
+
+
+== Description ==
+
+The Uploads Folder plugin for WordPress changes your uploads folders to a more natural uploads/yyyy/mm for posts (based on the post's date rather than the current date), and uploads/page/sub-page for static pages (based on the page's position in the hierarchy).
+
+
+== Installation ==
+
+1. Upload the plugin folder to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+
+== Frequently Asked Questions ==
+
+= Help Me! =
+
+The [Semiologic forum](http://forum.semiologic.com) is the best place to report issues. Please note, however, that while community members and I do our best to answer all queries, we're assisting you on a voluntary basis.
+
+If you require more dedicated assistance, consider using [Semiologic Pro](http://www.getsemiologic.com).
---
+++ uploads-folder.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Plugin Name: Uploads Folder
+Plugin URI: http://www.semiologic.com/software/wp-tweaks/uploads-folder/
+Description: Changes your uploads folders to a more natural uploads/yyyy/mm for posts (based on the post's date rather than the current date), and uploads/page/sub-page for static pages (based on the page's position in the hierarchy).
+Author: Denis de Bernardy
+Version: 1.0
+Author URI: http://www.semiologic.com
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts (http://www.mesoconcepts.com), and is distributed under the terms of the GPL license, v.2.
+
+http://www.opensource.org/licenses/gpl-2.0.php
+**/
+
+
+class uploads_folder
+{
+	#
+	# init()
+	#
+	
+	function init()
+	{
+		add_filter('upload_dir', array('uploads_folder', 'filter'));
+	} # init()
+	
+	
+	#
+	# filter()
+	#
+	
+	function filter($uploads)
+	{
+		$post_id = $_POST['post_id'];
+		
+		if ( !isset($post_id) || $post_id < 0 )
+		{
+			return $uploads;
+		}
+
+		$post = get_post($post_id);
+		
+		if ( $post->post_type == 'page' )
+		{
+			if ( $post->post_name == '' )
+			{
+				$post->post_name = sanitize_title($post->post_title);
+				
+				if ( $post->post_name == '' )
+				{
+					$post->post_name = $post->ID;
+				}
+			}
+			
+			$subdir = '/' . $post->post_name;
+			
+			if ( $post->post_parent <> 0 )
+			{
+				do
+				{
+					$post = get_post($post->post_parent);
+					
+					if ( $post->post_name == '' )
+					{
+						$post->post_name = sanitize_title($post->post_title);
+				
+						if ( $post->post_name == '' )
+						{
+							$post->post_name = $post->ID;
+						}
+					}
+					
+					$subdir = '/' . $post->post_name . $subdir;
+				} while ( $post->post_parent > 0 );
+			}
+		}
+		elseif ( $post->post_date != '0000-00-00 00:00:00' )
+		{
+			$subdir = date("/Y/m", strtotime($post->post_date));
+		}
+		
+		if ( $subdir && $subdir != $uploads['subdir'] )
+		{
+			$uploads['path'] = preg_replace("#" . $uploads['subdir'] . "$#", $subdir, $uploads['path']);
+			$uploads['url'] = preg_replace("#" . $uploads['subdir'] . "$#", $subdir, $uploads['url']);
+			$uploads['subdir'] = $subdir;
+			
+			if ( !wp_mkdir_p($uploads['path']) )
+			{
+				$message = sprintf( __( 'Unable to create directory %s. Is its parent directory writable by the server?' ), $uploads['path'] );
+				return array( 'error' => $message );
+			}
+		}
+		
+		return $uploads;
+	} # filter()
+} # uploads_folder
+
+uploads_folder::init();
+?>
