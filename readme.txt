876ef66ed72036d01ea980d5e5a86b3d7a7f6b24
Mike Koepke <mike.koepke@gmail.com>
2013-08-09 21:01:45 -0500

    - WP 3.6 compat - PHP 5.4 compat

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Denis-de-Bernardy, Mike_Koepke
 Donate link: http://www.semiologic.com/partners/
 Tags: uploads, uploads-folder, uploads folder, cms, semiologic
 Requires at least: 3.1
-Tested up to: 3.5.1
+Tested up to: 3.6
 Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -29,6 +29,11 @@ If you require more dedicated assistance, consider using [Semiologic Pro](http:/
 
 
 == Change Log ==
+
+= 2.2 =
+
+- WP 3.6 compat
+- PHP 5.4 compat
 
 = 2.1.1 =
 
--- uploads-folder.php
+++ uploads-folder.php
@@ -3,7 +3,7 @@
 Plugin Name: Uploads Folder
 Plugin URI: http://www.semiologic.com/software/uploads-folder/
 Description: Changes your uploads' subfolders to a more natural yyyy/mm/post-slug for posts (based on the post's date rather than the current date), and page-slug/subpage-slug for static pages (based on the page's position in the hierarchy).
-Version: 2.1.1
+Version: 2.2
 Author: Denis de Bernardy & Mike Koepke
 Author URI: http://www.getsemiologic.com
 Text Domain: uploads-folder
@@ -23,6 +23,4 @@ if ( !class_exists('uploads_folder') ) {
 	include dirname(__FILE__) . '/uploads-folder/uploads-folder.php';
 }
 
-register_activation_hook(__FILE__, array('uploads_folder', 'reset'));
-register_deactivation_hook(__FILE__, array('uploads_folder', 'reset'));
 ?>
